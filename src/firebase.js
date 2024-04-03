de66410821f20cf7c357a72f9a9abee7b26a49bc
yogendra sharma <yogendra.sh@cisinlabs.com>
2024-04-03 18:26:57 +0530

    removed the config from firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, onValue, child, orderByChild, equalTo, get, query } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCOr3nuKj-lJnf5Z7BfypcllF-j5GK8kE4",
-    authDomain: "teta-career-portal-testingdb.firebaseapp.com",
-    databaseURL: "https://teta-career-portal-testingdb-default-rtdb.firebaseio.com",
-    projectId: "teta-career-portal-testingdb",
-    storageBucket: "teta-career-portal-testingdb.appspot.com",
-    messagingSenderId: "896592933036",
-    appId: "1:896592933036:web:5eaecc03c1d4bad4bd6f09"
+
 };
 
 const app = initializeApp(firebaseConfig);
