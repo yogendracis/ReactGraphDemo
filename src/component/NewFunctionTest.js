de7f4ccc578d6dfe57703d2f65a7d611618b6a0e
yogendra sharma <yogendra.sh@cisinlabs.com>
2024-04-05 16:37:01 +0530

    adding new function file in github

---
+++ src/component/NewFunctionTest.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NewFunctionTest()
+{
+    return (
+        <div>
+            <h1>New push testing.</h1>
+            <p>From local to github.com</p>
+        </div>
+    )
+}
+
+export default NewFunctionTest
